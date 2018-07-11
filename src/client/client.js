90faa44dc44ae4091601fa09158795b3ba7906c3
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-07-11 11:20:11 +0900

    Add Route setting

--- src/client/client.js
+++ src/client/client.js
@@ -1,9 +1,11 @@
 // Startup point for the client side application
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home'
-
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
 ReactDOM.hydrate(
-  <Home />, document.getElementById('root')
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>, document.getElementById('root')
 );
