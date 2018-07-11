1ae1ecdf3aa80c747e3521fb67f32984a0aa45c7
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-07-11 17:14:18 +0900

    Add babel-polifill

--- src/client/client.js
+++ src/client/client.js
@@ -1,4 +1,5 @@
 // Startup point for the client side application
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,8 +7,9 @@ import Routes from './Routes';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import reducers from './reducers'
 
-const store = createStore(reducers, {} applyMiddleware(thunk))
+const store = createStore(reducers, {} , applyMiddleware(thunk))
 
 
 ReactDOM.hydrate(
--- src/client/reducers/usersReducer.js
+++ src/client/reducers/usersReducer.js
@@ -1,7 +1,6 @@
 import { FETCH_USERS } from '../actions'
 
-
-export default (state = [], atcion) => {
+export default (state = [], action) => {
   switch (action.type) {
     case FETCH_USERS:
       return action.payload.data;
